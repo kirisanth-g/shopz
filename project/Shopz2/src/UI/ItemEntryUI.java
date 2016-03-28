7d4e38e4a03373316b1d9448e8e03eae254fe47e
TerenceLai <developer.terence.77@gmail.com>
2016-03-28 02:53:39 -0400

    Made the src more readable (not really important commit ignore )

--- project/Shopz2/src/UI/ItemEntryUI.java
+++ project/Shopz2/src/UI/ItemEntryUI.java
@@ -26,33 +26,33 @@ public class ItemEntryUI extends Composite implements View {
 		setLayout(new GridLayout(3, false));
 
 		itemId = new Label(this, SWT.NONE);
-								
-										category = new Label(this, SWT.NONE);
-						
-								btnAddToCart = new Button(this, SWT.NONE);
-								btnAddToCart.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-								btnAddToCart.addSelectionListener(new SelectionAdapter() {
-									@Override
-									public void widgetSelected(SelectionEvent e) {
-										// put add to cart function here
-									}
-								});
-						
-								itemName = new Label(this, SWT.NONE);
-								new Label(this, SWT.NONE);
-						
-								price = new Label(this, SWT.NONE);
-								price.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-						
-								manufacturer = new Label(this, SWT.NONE);
-				new Label(this, SWT.NONE);
-				new Label(this, SWT.NONE);
-		
-				desc = new Text(this, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL);
-				GridData gd_text = new GridData(SWT.FILL, SWT.TOP, true, false, 3, 1);
-				gd_text.widthHint = 440;
-				gd_text.heightHint = 52;
-				desc.setLayoutData(gd_text);
+
+		category = new Label(this, SWT.NONE);
+
+		btnAddToCart = new Button(this, SWT.NONE);
+		btnAddToCart.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		btnAddToCart.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// put add to cart function here
+			}
+		});
+
+		itemName = new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+
+		price = new Label(this, SWT.NONE);
+		price.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+
+		manufacturer = new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+
+		desc = new Text(this, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP | SWT.V_SCROLL);
+		GridData gd_text = new GridData(SWT.FILL, SWT.TOP, true, false, 3, 1);
+		gd_text.widthHint = 440;
+		gd_text.heightHint = 52;
+		desc.setLayoutData(gd_text);
 
 		// sample call
 		resetView();
@@ -69,8 +69,7 @@ public class ItemEntryUI extends Composite implements View {
 		manufacturer.setText("Manufacturer");
 		category.setText("category");
 		price.setText("price");
-		desc.setText(
-				"This is supposed to be a really long line of text. Supports auto "
+		desc.setText("This is supposed to be a really long line of text. Supports auto "
 				+ "wrapping and scrolling But i don't if this wokrs, I guess I have to type"
 				+ " a little bit more to see what will happen on this text bar. I'm running out of"
 				+ " energy at this rate. all i neeed now is a little more text");
