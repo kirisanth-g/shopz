f568d7a129f44b3de2ec78ebb1d21bdb27ddf84d
Helen Zhou <hel.zhou@mail.utoronto.ca>
2016-03-28 21:14:08 -0400

    more work of reworked User.java

--- project/Shopz2/src/LogicalLayer/User.java
+++ project/Shopz2/src/LogicalLayer/User.java
@@ -25,9 +25,6 @@ public class User {
 	private int ccv;
 	private String cardType;
 	
-	//Review
-	private ArrayList<Review> reviews;
-	
 	//ShoppingCart
 	private ArrayList<Item> cart;
 	
@@ -59,7 +56,7 @@ public class User {
 		}
 		
 		//get and store info from Address
-		con.sqlQuery(String.format("SELECT * FROM Address WHERE username=%s", this.username));
+		con.sqlQuery(String.format("SELECT * FROM Address WHERE user=%s", this.username));
 		try {
 			result = con.getResult();
 			result.next();
@@ -71,9 +68,43 @@ public class User {
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		
+		}		
 		
 		//get and store info from payment
+		con.sqlQuery(String.format("SELECT * FROM PaymentInfo WHERE username=%s", this.username));
+		try {
+			result = con.getResult();
+			result.next();
+			
+			cardnum = result.getInt("cardNumber");
+			expDate = result.getString("expDate");
+			ccv = result.getInt("ccv");
+			cardType = result.getString("cardType");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//get and store info from ShoppingCart
+		con.sqlQuery(String.format("SELECT * FROM (Item INNER JOIN "
+				+ "(SELECT * FROM ShoppingCart WHERE Username=%s)) ON "
+				+ "(ShoppingCart.item=Item.itemID)", this.username));
+		try {
+			result = con.getResult();
+			while(result.next()){
+				cart.add(new Item(result.getString("itemID"), result.getString("name"), result.getString("manufacturer"),
+						result.getString("description"), result.getString("category"), result.getFloat("price")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args){
+		DBConnector con = new DBConnector("localhost", "3306", "root", "admin", "Shopz");
+		
+		con.DDLStatement("INSERT INTO User VALUES ('user2', 'bear', 'boo', 0)");
 	}
 }
